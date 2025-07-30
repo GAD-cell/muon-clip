1ef3305d0cea9749ec264b53033a122655b41398
GAD-cell <gadsinoue@gmail.com>
2025-07-30 14:32:32 +0200

    working qk-clipping

--- src/test/test.py
+++ src/test/test.py
@@ -6,51 +6,30 @@ import re
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from muon_pp import MuonPP
 
-
-os.environ["HF_TOKEN"] = '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'
-
 model_name = "Qwen/Qwen3-0.6B"  
 tokenizer = AutoTokenizer.from_pretrained(model_name)
-#config = AutoConfig.from_pretrained(model_name)
-#print(config)
-
+config = AutoConfig.from_pretrained(model_name)
 model = AutoModelForCausalLM.from_pretrained(
     model_name,
     device_map="auto",  
     torch_dtype=torch.bfloat16,
     cache_dir="./model_cache"
 )
+optimizer = MuonPP(model, config)
 
 model.train()
-
-from utils.utils import make_proj_hook, make_self_attn_hook, attn_outputs
-
-
-for name, module in model.named_modules():
-
-    if m := re.match(r".*layers\.(\d+)\.self_attn\.(q_proj|k_proj)$", name):
-        layer_idx = int(m.group(1))
-        proj_type = m.group(2)[0]
-        module.register_forward_hook(make_proj_hook(layer_idx, proj_type))
-
-    elif m := re.match(r".*layers\.(\d+)\.self_attn$", name):
-        layer_idx = int(m.group(1))
-        module.register_forward_hook(make_self_attn_hook(layer_idx))
-
-
 prompt = "The capital of France is"
 inputs = tokenizer(prompt, return_tensors="pt").to(model.device)
 labels = inputs["input_ids"].clone()
 inputs["labels"] = labels
 
-# Passer dans le modèle
+
 outputs = model(**inputs)
 loss = outputs.loss
 
 print(f"Dummy loss: {loss.item()}")
 
-# Optimiseur custom
-optimizer = MuonPP(list(model.named_parameters()))
+# Optimizer
 optimizer.zero_grad()
 loss.backward()
 optimizer.step()
--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,4 +1,5 @@
 import torch
+import re
 
 def newtonschulz(G, steps=5, eps=1e-7):
     a, b, c = (3.4445, -4.7750, 2.0315)
@@ -13,11 +14,6 @@ def newtonschulz(G, steps=5, eps=1e-7):
     if transpose:
         X = X.T
     return X
-
-
-def qkclip(G):
-    pass
-
 
 def adam_update(grad, buf1, buf2, step, betas, eps):
     buf1.lerp_(grad, 1 - betas[0])
@@ -36,53 +32,63 @@ def muon_update(grad, momentum, beta=0.95, ns_steps=5, nesterov=True):
     update *= max(1, grad.size(-2) / grad.size(-1))**0.5
     return update
 
-def proj_hook(module, input, output):
-    
-    pass
 
-
-def max_clip_hook(module, input, output):
-    pass
-
-
-def repeat_kv(hidden_states: torch.Tensor, n_rep: int) -> torch.Tensor:
+def repeat_kv(hidden_states: torch.Tensor, n_rep: int, num_key_value_heads: int, head_dim: int ) -> torch.Tensor:
     """
-    This is the equivalent of torch.repeat_interleave(x, dim=1, repeats=n_rep). The hidden states go from (batch,
-    num_key_value_heads, seqlen, head_dim) to (batch, num_attention_heads, seqlen, head_dim)
+    For KV-cache. repeat k_proj to perform q_proj*k_proj.T 
     """
-    batch, slen, head_dim = hidden_states.shape
-    num_key_value_heads = 8
-    head_dim = 128
+    batch, slen, _ = hidden_states.shape
 
-    if n_rep == 1:
-        return hidden_states
+    if n_rep == 1: #num_attention_head==num_key_value_heads
+        return hidden_states.view(batch, slen, num_key_value_heads, head_dim)
     
     hidden_states = hidden_states.view(batch, slen, num_key_value_heads, -1, head_dim)    
     hidden_states = hidden_states.permute(0, 2, 3, 1, 4) 
     hidden_states = hidden_states[:, :, :, :, :].expand(batch, num_key_value_heads, n_rep, slen, head_dim)
     return hidden_states.reshape(batch, num_key_value_heads * n_rep, slen, head_dim)
 
-# === Déclaration du cache global ===
-attn_outputs = {}
 
-# === Fonctions de hooks ===
-def make_proj_hook(layer_idx, proj_type):
-    def hook(module, input, output):
-        print(module)
-        if layer_idx not in attn_outputs:
-            attn_outputs[layer_idx] = {}
-        attn_outputs[layer_idx][proj_type] = output
-    return hook
+class HookRecorder:
+    def __init__(self):
+        self.attn_inputs = {}
 
-def make_self_attn_hook(layer_idx):
-    def hook(module, input, output):
-        q = attn_outputs.get(layer_idx, {}).get("q")
-        k = attn_outputs.get(layer_idx, {}).get("k")
-        q = repeat_kv(q,2) # n_rep = 2 = num_attention_heads // num_key_value_heads
-        k = repeat_kv(k,2)
-        if q is not None and k is not None:
-            attn_logits = torch.matmul(q, k.transpose(-1, -2))
-            print(f"[Layer {layer_idx}] max(QK^T): {attn_logits.max().item():.4f}")
-    return hook
+    def make_proj_input_hook(self, layer_idx, proj_type):
+        def hook(module, input, output):
+            if layer_idx not in self.attn_inputs:
+                self.attn_inputs[layer_idx] = {}
+            self.attn_inputs[layer_idx] = input[0]
+        return hook
+
+    def register_input_hook(self, model):
+        found_layer = False
+        for name, module in model.named_modules():
+            if m := re.match(r".*layers\.(\d+)\.self_attn\.(q_proj)$", name):
+                layer_idx = int(m.group(1))
+                proj_type = m.group(2)[0]
+                module.register_forward_hook(self.make_proj_input_hook(layer_idx, proj_type))
+                found_layer = True
+        return found_layer
+
+hook_recorder = HookRecorder()
+
+
+# def make_self_attn_hook(layer_idx, alpha, t):
+#     def hook(module, input, output):
+#         q = attn_outputs.get(layer_idx, {}).get("q")
+#         k = attn_outputs.get(layer_idx, {}).get("k")
+#         q = repeat_kv(q,2) # n_rep = 2 = num_attention_heads // num_key_value_heads
+#         k = repeat_kv(k,2)
+#         if q is not None and k is not None:
+#             attn_logits = torch.matmul(q, k.transpose(-1, -2))
+#             max_logits = attn_logits.max()
+
+#             print(f"[Layer {layer_idx}] max(QK^T): {attn_logits.max().item():.4f}")
+
+#         eta = min(t/max_logits,1)
+#         with torch.no_grad():
+#             module.q_proj.weight.mul_(torch.pow(eta, alpha))
+#             module.k_proj.weight.mul_(torch.pow(eta, 1 - alpha))
+
+#     return hook
 
 
